99e9c847309977efbeefae041fa48d19d5972a0e
seungmin-park <tmddudals369@naver.com>
2022-05-03 23:39:38 +0900

    project optimize imports

--- src/main/java/com/inhatc/demp/repository/AnnouncementQueryRepository.java
+++ src/main/java/com/inhatc/demp/repository/AnnouncementQueryRepository.java
@@ -6,14 +6,12 @@ import com.inhatc.demp.dto.announcement.AnnouncementSearchCondition;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
--- src/test/java/com/inhatc/demp/repository/AnnouncementQueryRepositoryTest.java
+++ src/test/java/com/inhatc/demp/repository/AnnouncementQueryRepositoryTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
