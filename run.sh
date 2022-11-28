588029407e1a90bff59ba951e2541b6352b8ba29
박승민 <tmddudals369@naver.com>
2022-11-28 21:15:23 +0900

    fix: qoddi 명령어 추가

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 echo ${SECURE_FILE} >> env.json
 sed 's/*/"/g' env.json >> secureFile.json
+qoddi buildpacks:set https://github.com/qoddi/qoddi-buildpack-gradle#previous-version
 java -Dserver.port=$PORT $JAVA_OPTS -Dspring.profiles.active=prod -jar ./build/libs/demp-0.0.1-SNAPSHOT.jar
